70a23929d0480c5e4a5f9449dfc1fbd8018c782c
Uday Kumar <udaykumar5706@gmail.com>
2025-06-08 07:54:10 +0530

    Initial Commit

--- app/[handle]/page.js
+++ app/[handle]/page.js
@@ -47,7 +47,7 @@ const HandlePage = async ({ params }) => {
         {/* Links List */}
         <div className="flex flex-col mt-8 items-center gap-2 w-[40%]">
           {Links.map((item, index) => (
-            <div key={index} className="text-left bg-white/80 p-5 flex hover:scale-[1.05] transition-transform duration-300 justify-center w-full rounded-md">
+            <div key={index} className="text-left overflow-x-auto smooth bg-white/80 p-5 flex hover:scale-[1.05] transition-transform duration-300 justify-center w-full rounded-md">
               <span className="font-medium">{item.linktext}: <a
                 href={item.link}
                 target="_blank"
